2405a531d67bfd1051e8e23d8612fb4f265080cb
Ray Hu <raymond.hu05@imperial.ac.uk>
2017-01-03 16:12:09 +0000

    Continued.

--- modules/core/src/main/java/org/scribble/ext/f17/del/AnnotUnaryPayloadElemDel.java
+++ modules/core/src/main/java/org/scribble/ext/f17/del/AnnotUnaryPayloadElemDel.java
@@ -3,6 +3,7 @@ package org.scribble.ext.f17.del;
 import org.scribble.ast.ScribNode;
 import org.scribble.del.ScribDelBase;
 import org.scribble.ext.f17.ast.AnnotUnaryPayloadElem;
+import org.scribble.ext.f17.sesstype.name.PayloadVar;
 import org.scribble.main.ScribbleException;
 import org.scribble.sesstype.kind.DataTypeKind;
 import org.scribble.visit.wf.NameDisambiguator;
@@ -12,7 +13,13 @@ public class AnnotUnaryPayloadElemDel extends ScribDelBase
 	@Override
 	public void enterDisambiguation(ScribNode parent, ScribNode child, NameDisambiguator disamb) throws ScribbleException
 	{
+		// N.B. payload params counted as protodecl params, so cannot shadow those
 		AnnotUnaryPayloadElem<?> pe = (AnnotUnaryPayloadElem<?>) child;
-		disamb.addParameter(pe.payvar.toName(), DataTypeKind.KIND);
+		PayloadVar pv = pe.payvar.toName();
+		if (disamb.isBoundParameter(pv))
+		{
+			throw new ScribbleException("[f17] Duplicate payload var: " + pv);  // FIXME: probably doesn't check across protodecls
+		}
+		disamb.addParameter(pv, DataTypeKind.KIND);
 	}
 }
