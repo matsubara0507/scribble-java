2a96ce5a48ef1e03d014e967f2fff7fe1a7be2ce
Ray Hu <raymond.hu05@imperial.ac.uk>
2017-01-19 17:51:03 +0000

    Tidying f17 examples.

--- modules/f17/src/test/scrib/demo/fase17/travel/orig/Travel1C.java
+++ modules/f17/src/test/scrib/demo/fase17/travel/orig/Travel1C.java
@@ -8,11 +8,6 @@ import static demo.fase17.travel.orig.Travel1.Travel.Travel.confirm;
 import static demo.fase17.travel.orig.Travel1.Travel.Travel.pay;
 import static demo.fase17.travel.orig.Travel1.Travel.Travel.query;
 import static demo.fase17.travel.orig.Travel1.Travel.Travel.quote;
-import static demo.fase17.travel.orig.Travel1.Travel.Travel.reject;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import org.scribble.net.Buf;
 import org.scribble.net.ObjectStreamFormatter;
@@ -26,8 +21,7 @@ import demo.fase17.travel.orig.Travel1.Travel.roles.C;
 
 public class Travel1C
 {
-	private static final int MAX = 500;
-	private static final List<String> QUERIES = IntStream.range(97, 122).mapToObj((i) -> new Character((char) i).toString()).collect(Collectors.toList());
+	private static final String[] queries = new String[] { "aaa", "bbb", "ccc", "ddd" };
 	
 	public static void main(String[] args) throws Exception
 	{
@@ -35,66 +29,18 @@ public class Travel1C
 		try (ExplicitEndpoint<Travel, C> se
 				= new ExplicitEndpoint<>(travel, C, new ObjectStreamFormatter()))
 		{
-			run(new Travel_C_1(se).connect(A, SocketChannelEndpoint::new, "localhost", 8888),
-					QUERIES);
+			Buf<Integer> b = new Buf<>();
+			Travel_C_2 C2 = new Travel_C_1(se).connect(A, SocketChannelEndpoint::new, "localhost", 8888);
+			for (int i = 0; i < queries.length; i++)
+			{
+				C2 = C2.send(A, query, queries[0]) .receive(A, quote, b);
+			}
+				C2.connect(S, SocketChannelEndpoint::new, "localhost", 9999)
+					.send(S, pay, "...")
+					.receive(S, confirm, b)
+					.send(A, accpt, b.val);
+
+			System.out.println("Done: " + b.val);
 		}
 	}
-
-	private static void run(Travel_C_2 s2, List<String> qs) throws Exception
-	{
-		if (qs.isEmpty())
-		{
-			System.out.println("Rejecting: ");
-			s2.send(A, reject);
-		}	
-		else
-		{
-			System.out.println("Querying: " + qs.get(0));
-			Buf<Integer> b = new Buf<>();
-			s2 = s2.send(A, query, qs.get(0))
-			       .receive(A, quote, b);
-			System.out.println("Quoted: " + b.val);
-			if (b.val > MAX)
-			{
-				run(s2, qs.subList(1, qs.size()));
-			}
-			else
-			{
-				System.out.println("Accepting: ");
-				s2.connect(S, SocketChannelEndpoint::new, "localhost", 9999)
-					.send(S, pay, "...")
-					.receive(S, confirm, b)
-					.send(A, accpt, b.val);
-				System.out.println("Done: " + b.val);
-			}
-		}
-	}	
-
-	/*private static void run(Travel_C_2 s2) throws Exception
-	{
-		Buf<Integer> b = new Buf<>();
-		for (int i = 0; ; i++)
-		{
-			if (i >= QUERIES.size())
-			{
-				System.out.println("Rejecting: ");
-				s2.send(A, reject);
-				break;
-			}
-			System.out.println("Querying: " + QUERIES.get(i));
-			s2 = s2.send(A, query, QUERIES.get(i))
-						 .receive(A, quote, b);
-			System.out.println("Quoted: " + b.val);
-			if (b.val <= MAX)
-			{
-				System.out.println("Accepting: ");
-				s2.connect(S, SocketChannelEndpoint::new, "localhost", 9999)
-					.send(S, pay, "...")
-					.receive(S, confirm, b)
-					.send(A, accpt, b.val);
-				break;
-			}
-		}
-		System.out.println("Done: " + b.val);
-	}*/
 }
