7e18bd66f6b45dcb31ef4acc6d0fa731fad75852
Ray Hu <raymond.hu@imperial.ac.uk>
2017-07-13 13:24:12 +0100

    Hacked root pom exclude for f17.  Fixed F17AllTest.

--- scribble-f17/src/test/java/org/scribble/ext/f17/cli/F17AllTest.java
+++ scribble-f17/src/test/java/org/scribble/ext/f17/cli/F17AllTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.scribble.cli.CLArgParser;
-import org.scribble.cli.CommandLine;
 import org.scribble.cli.CommandLineException;
 import org.scribble.cli.ScribAllTest;
 import org.scribble.ext.f17.main.F17SyntaxException;
@@ -28,6 +27,13 @@ public class F17AllTest extends ScribAllTest
 	{
 		super(example, isBadTest);
 	}
+	
+	// FIXME: base class should not specify "."
+	@Override
+	protected String getTestRootDir()
+	{
+		return "../../../scribble-test/target/test-classes/";
+	}
 
 	@Override
 	@Test
@@ -44,9 +50,12 @@ public class F17AllTest extends ScribAllTest
 			
 			String[] SKIP =  // HACK
 				{
-					"modules/cli/target/test-classes/bad/wfchoice/enabling/twoparty/Test01b.scr",  // f17 doesn't check choice subjects
-					"modules/cli/target/test-classes/bad/wfchoice/gchoice/Choice02.scr",
-					//"modules/cli/target/test-classes/bad/wfchoice/enabling/fourparty/Test01.scr"  // The original choice subject problem is gone, but we get a role-progress error instead (without fairness)
+					// f17 doesn't check choice subjects
+					"scribble-test/target/test-classes/bad/wfchoice/enabling/twoparty/Test01b.scr",
+					"scribble-test/target/test-classes/bad/wfchoice/gchoice/Choice02.scr",
+
+					// The original choice subject problem is gone, but we get a role-progress error instead (without fairness)
+					//"scribble-test/target/test-classes/bad/wfchoice/enabling/fourparty/Test01.scr"
 				};
 			String foo = this.example.replace("\\", "/");
 			for (String skip : SKIP)
@@ -60,8 +69,8 @@ public class F17AllTest extends ScribAllTest
 			}
 			
 			//new CommandLine(this.example, CommandLineArgParser.JUNIT_FLAG, CommandLineArgParser.IMPORT_PATH_FLAG, dir).run();
-			new CommandLine(this.example, CLArgParser.JUNIT_FLAG, CLArgParser.IMPORT_PATH_FLAG, dir, 
-						CLArgParser.F17_FLAG, "[F17AllTest]")  // HACK (cf. F17Main)
+			new F17CommandLine(this.example, CLArgParser.JUNIT_FLAG, CLArgParser.IMPORT_PATH_FLAG, dir, 
+						F17CLArgParser.F17_FLAG, "[F17AllTest]")  // HACK (cf. F17Main)
 					.run();
 			Assert.assertFalse("Expecting exception", this.isBadTest);
 		}
@@ -72,7 +81,7 @@ public class F17AllTest extends ScribAllTest
 		}
 		catch (ScribbleException e)
 		{
-			Assert.assertTrue("Unexpected exception '" + e.getMessage() + "'", this.isBadTest);
+			Assert.assertTrue("Unexpected exception '\n" + ClassLoader.getSystemResource(getTestRootDir()).getFile() + "\n" + e.getMessage() + "'", this.isBadTest);
 		}
 		//catch (ScribParserException | CommandLineException e)
 		catch (CommandLineException e)
